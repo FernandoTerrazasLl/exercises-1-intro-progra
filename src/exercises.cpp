1646053dae55eb7a5850580159545e41914e1524
FernandoTerrazasLl <159574670+FernandoTerrazasLl@users.noreply.github.com>
2024-03-08 12:04:26 -0500

    corrigiendo exercise 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -7,11 +7,13 @@ using namespace std;
 void exercise_1(string s1, string s2, string s3) {
   if(s1 <= s2 && s1 <= s3){
         cout << s1;
-        
-    } else if(s2 <= s1 && s2 <=s3) {
+
+    } else if(s2 <= s1 && s2 <= s3) {
         cout << s2;
+
     } else{
         cout << s3;
+    }
 }
 
 void exercise_2(double A, double B, double C) {
