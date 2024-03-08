c8aa0536a169a7ccbd092b5dbf06b4df97d889ff
FernandoTerrazasLl <159574670+FernandoTerrazasLl@users.noreply.github.com>
2024-03-08 12:01:04 -0500

    Enviando exercise 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,7 +5,13 @@
 using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
-  cout << "Buen dia" << endl;
+  if(s1 <= s2 && s1 <= s3){
+        cout << s1;
+        
+    } else if(s2 <= s1 && s2 <=s3) {
+        cout << s2;
+    } else{
+        cout << s3;
 }
 
 void exercise_2(double A, double B, double C) {
